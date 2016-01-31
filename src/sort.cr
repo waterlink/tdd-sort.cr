ebea27289043ac3aa4949d299683a98686813f88
Oleksii Fedorov <waterlink000@gmail.com>
2016-01-31 23:46:40 +0100

    [RED] Change 25

--- src/sort.cr
+++ src/sort.cr
@@ -2,12 +2,14 @@ module Sort
   def self.sort(a)
     return a if a.size < 2
 
-    if a[0] > a[1]
-      a[0], a[1] = {a[1], a[0]}
+    i = 0
+    if a.size > i + 1 && a[i] > a[i + 1]
+      a[i], a[i + 1] = {a[i + 1], a[i]}
     end
 
-    if a.size > 2 && a[1] > a[2]
-      a[1], a[2] = {a[2], a[1]}
+    i = 1
+    if a.size > i + 1 a[i] > a[i + 1]
+      a[i], a[i + 1] = {a[i + 1], a[i]}
     end
 
     a
