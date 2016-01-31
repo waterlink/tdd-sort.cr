53a2570096b9569581394ec46e79221ddebf4fe3
Oleksii Fedorov <waterlink000@gmail.com>
2016-02-01 00:13:57 +0100

    [RED] Change 71

--- src/immutable_sort.cr
+++ src/immutable_sort.cr
@@ -2,13 +2,10 @@ module ImmutableSort
   def self.sort(a)
     return a if a.size < 2
 
-    left = [] of Int32
-    right = [] of Int32
     if a[0] > a[1]
-      left << a[1]
-      return left + [a[0]] + a[2..-1]
+      return [a[1], a[0]] + a[2..-1]
     end
 
-    [a[0]] + sort(a[1..-1])
+    [a[0]] + a[]
   end
 end
